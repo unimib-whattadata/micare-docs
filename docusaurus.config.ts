b26dac7dcb35ec0a5106801f3d5f3b03d6b0d862
Elia Guarnieri <elia.guarnieri@gmail.com>
2025-02-11 17:15:50 +0100

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Unimib Whattadata', // Usually your GitHub org/user name.
-  projectName: 'MiCare Docs', // Usually your repo name.
+  projectName: 'LLMind Docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -69,9 +69,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'MiCare Docs',
+      title: 'LLMind Docs',
       logo: {
-        alt: 'MiCare Logo',
+        alt: 'LLMind Logo',
         src: 'img/logo.svg',
       },
       items: [
